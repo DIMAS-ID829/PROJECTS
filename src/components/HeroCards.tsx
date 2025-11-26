bdda501b2d3854fd9878bdc319b323abeabff8cc
DIMAS ID <dimas.kurnia829@smk.belajar.id>
2025-11-26 17:08:16 +0700

    .

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -108,47 +108,74 @@ export const HeroCards = () => {
         </CardFooter>
       </Card>
 
-      {/* Pricing */}
-      <Card className="absolute top-[150px] left-[50px] w-72  drop-shadow-xl shadow-black/10 dark:shadow-white/10">
-        <CardHeader>
-          <CardTitle className="flex item-center justify-between">
-            Free
-            <Badge
-              variant="secondary"
-              className="text-sm text-primary"
-            >
-              XII
-            </Badge>
-          </CardTitle>
-          <div>
-            <span className="text-3xl font-bold"></span>
-            <span className="text-muted-foreground"></span>
-          </div>
-
-          <CardDescription>
-            .
+      {/* Team */}
+      <Card className="absolute right-[20px] top-4 w-80 flex flex-col justify-center items-center drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <CardHeader className="mt-8 flex justify-center items-center pb-2">
+          <img
+            src="https://i.pravatar.cc/150?img=58"
+            alt="user avatar"
+            className="absolute grayscale-[0%] -top-12 rounded-full w-24 h-24 aspect-square object-cover"
+          />
+          <CardTitle className="text-center">DIMAS ID</CardTitle>
+          <CardDescription className="font-normal text-primary">
+            Frontend Developer
           </CardDescription>
         </CardHeader>
 
-        <CardContent>
-          <Button className="w-full">Go To</Button>
+        <CardContent className="text-center pb-2">
+          <p>
+            I really enjoy transforming ideas into functional software that
+            exceeds expectations
+          </p>
         </CardContent>
 
-        <hr className="w-4/5 m-auto mb-4" />
+        <CardFooter>
+          <div>
+            <a
+              rel="noreferrer noopener"
+              href="https://github.com/DIMAS-ID829/PROJECTS"
+              target="_blank"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              <span className="sr-only">Github icon</span>
+              <GitHubLogoIcon className="w-5 h-5" />
+            </a>
+            <a
+              rel="noreferrer noopener"
+              href="https://twitter.com/leo_mirand4"
+              target="_blank"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              <span className="sr-only">X icon</span>
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="fill-foreground w-5 h-5"
+              >
+                <title>X</title>
+                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+              </svg>
+            </a>
 
-        <CardFooter className="flex">
-          <div className="space-y-4">
-            {[""].map(
-              (benefit: string) => (
-                <span
-                  key={benefit}
-                  className="flex"
-                >
-                  <Check className="text-green-500" />{" "}
-                  <h3 className="ml-2">{benefit}</h3>
-                </span>
-              )
-            )}
+            <a
+              rel="noreferrer noopener"
+              href="https://www.linkedin.com/in/leopoldo-miranda/"
+              target="_blank"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              <span className="sr-only">Linkedin icon</span>
+              <Linkedin size="20" />
+            </a>
           </div>
         </CardFooter>
       </Card>
